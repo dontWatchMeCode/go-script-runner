72d0bbacb13f5c5c2825b25594942df143542296
M. Gschwandtner <84477901+dontWatchMeCode@users.noreply.github.com>
2023-05-14 00:20:00 +0200

    updated _example.sh file

--- scripts/_example.sh
+++ scripts/_example.sh
@@ -1,6 +1,28 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/bash
 
-# if the script doesnt print to stdout / stderr, it wont be written to the log
+# example for updating a python project running in a docker container
+# the script only prints to stdout if there are changes to pull from the repo
 
-echo "this will be written to the log"
+TARGET_PATH=/some/path
+cd $TARGET_PATH || echo "ERROR: path not found" || exit
+
+git stash &>/dev/null
+
+# check if there are changes
+PULL=$(git pull)
+if [[ $PULL != *"Already up to date."* ]]; then
+    echo "INFO: git pull"
+
+    #check if docker is running
+    DOCKER_PS=$(docker compose ps)
+
+    # build the container
+    echo "INFO: docker compose build"
+    docker compose build
+
+    # start the container if it was running
+    if [[ $DOCKER_PS == *"python ./main.py"* ]]; then
+        echo "INFO: docker compose up"
+        docker compose up -d
+    fi
+fi
