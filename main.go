7ca14a6eb9672548e5936ea3f954adc1cba3f28e
M. Gschwandtner <gm@webprofil.at>
2023-05-15 22:30:41 +0200

    added test option for discord webhook

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"dontWatchMeCode/pipe/pkg/core"
+	"dontWatchMeCode/pipe/pkg/webhooks/discord"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -20,6 +22,13 @@ func main() {
 
 	if len(os.Args[1:]) != 0 && os.Args[1] == "-run" {
 		core.RunAllScript(pwd)
+	} else if len(os.Args[1:]) != 0 && os.Args[1] == "-test" {
+		fmt.Println("Sending discord webhook test")
+		discordWebhookUrl, err := core.GetEnv("DISCORD_WEBHOOK_URL")
+		if err != nil {
+			panic(err)
+		}
+		discord.CallDiscordWebhookTest(discordWebhookUrl)
 	} else {
 		signalChannel := make(chan os.Signal, 1)
 		signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
--- pkg/webhooks/discord/discord.go
+++ pkg/webhooks/discord/discord.go
@@ -54,3 +54,31 @@ func CallDiscordWebhook(data SendData) error {
 
 	return errors.New("Webhook request failed status:" + strconv.Itoa(resp.StatusCode))
 }
+
+func CallDiscordWebhookTest(url string) error {
+	return CallDiscordWebhook(
+		SendData{
+			Url: url,
+			Message: Message{
+				Content: "",
+				Embeds: []Embed{
+					{
+						Title:       "Title test 1",
+						Description: "Description test",
+						Color:       EmbedsColorGreen,
+					},
+					{
+						Title:       "Title test 2",
+						Description: "Description test",
+						Color:       EmbedsColorRed,
+					},
+					{
+						Title:       "Title test 3",
+						Description: "Description test",
+						Color:       EmbedsColorYellow,
+					},
+				},
+			},
+		},
+	)
+}
